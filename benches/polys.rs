3c624f3ae7afc6de8c0e7f25f0b3b9439942d72b
bibbinth <bobbinth@protonmail.com>
2020-03-27 02:06:35 -0700

    optimized quartic interpolation

--- benches/polys.rs
+++ benches/polys.rs
@@ -20,13 +20,24 @@ pub fn eval4(c: &mut Criterion) {
 }
 
 pub fn interpolate4_batch(c: &mut Criterion) {
-    let r = field::get_root_of_unity(1024);
-    let xs = field::get_power_series(r, 1024);
-    let mut ys = vec![0u64; 1024];
+    let n: usize = 1 << 10;
+    let r = field::get_root_of_unity((n * 4) as u64);
+    let xs = field::get_power_series(r, n * 4);
+    let mut ys = vec![0u64; n * 4];
     field::rand_fill(&mut ys);
     c.bench_function("Poly interpolation (quartic batch)", |bench| {
         bench.iter(|| quartic::interpolate_batch(black_box(&xs), black_box(&ys)))
     });
+    
+    /*
+    c.bench_function("Poly interpolation (quartic batch)", |bench| {
+        bench.iter(|| {
+            for i in (0..n).step_by(4) {
+                polys::interpolate(black_box(&xs[i..(i+4)]), black_box(&ys[i..(i+4)]));
+            }
+        })
+    });
+    */
 }
 
 criterion_group!(group, eval, eval4, interpolate4_batch);
--- src/math/quartic.rs
+++ src/math/quartic.rs
@@ -15,81 +15,109 @@ pub fn eval(p: &[u64], x: u64) -> u64 {
     return y;
 }
 
-pub fn interpolate_batch(x_sets: &[u64], y_sets: &[u64]) -> Vec<u64> {
+pub fn evaluate_batch(polys: &[u64], xs: &[u64]) -> Vec<u64> {
+    let n = polys.len() / 4;   // number of polynomials
+    debug_assert!(n == xs.len(), "number of polynomials must be equal to number of X coordinates");
 
-    let mut equations: Vec<u64> = Vec::with_capacity(x_sets.len() * 4);
-    let mut inverses: Vec<u64> = Vec::with_capacity(x_sets.len());
-    let mut eq_idx: usize = 0;
+    let mut result: Vec<u64> = Vec::with_capacity(n);
+    unsafe { result.set_len(n); }
 
-    for i in (0..x_sets.len()).step_by(4) {
-        let xs = &x_sets[i..(i + 4)];
+    for (i, j) in (0..n).zip((0..polys.len()).step_by(4)) {
+        result[i] = eval(&polys[j..(j + 4)], xs[i]);
+    }
+
+    return result;
+}
+
+/// Interpolates a set of X, Y coordinates into a batch of degree 3 polynomials; The coordinates are
+/// assumed to be in batches with 4 sequential coordinates per batch.
+/// 
+/// This function is many times faster than using `polys::interpolate` function in a loop. This is
+/// primarily due to the ability to do batch inversion.
+pub fn interpolate_batch(xs: &[u64], ys: &[u64]) -> Vec<u64> {
+    let n = xs.len() / 4;   // number of batches
+    debug_assert!(xs.len() == ys.len(), "number of X coordinates must be equal to number of Y coordinates");
+
+    let mut equations: Vec<u64> = Vec::with_capacity(n * 16);
+    let mut inverses: Vec<u64> = Vec::with_capacity(n * 4);
+    unsafe { 
+        equations.set_len(n * 16);
+        inverses.set_len(n * 4);
+    }
+
+    for (i, j) in (0..xs.len()).step_by(4).zip((0..equations.len()).step_by(16)) {
         
-        let x01 = field::mul(xs[0], xs[1]);
-        let x02 = field::mul(xs[0], xs[2]);
-        let x03 = field::mul(xs[0], xs[3]);
-        let x12 = field::mul(xs[1], xs[2]);
-        let x13 = field::mul(xs[1], xs[3]);
-        let x23 = field::mul(xs[2], xs[3]);
-    
-        // eq0
-        equations.push(field::mul(field::neg(x12), xs[3]));
-        equations.push(field::add(field::add(x12, x13), x23));
-        equations.push(field::sub(field::sub(field::neg(xs[1]), xs[2]), xs[3]));
-        equations.push(1);
+        let x01 = field::mul(xs[i + 0], xs[i + 1]);
+        let x02 = field::mul(xs[i + 0], xs[i + 2]);
+        let x03 = field::mul(xs[i + 0], xs[i + 3]);
+        let x12 = field::mul(xs[i + 1], xs[i + 2]);
+        let x13 = field::mul(xs[i + 1], xs[i + 3]);
+        let x23 = field::mul(xs[i + 2], xs[i + 3]);
 
-        inverses.push(eval(&equations[eq_idx..(eq_idx + 4)], xs[0]));
-        eq_idx += 4;
+        // eq0
+        equations[j + 0] = field::mul(field::neg(x12), xs[i + 3]);
+        equations[j + 1] = field::add(field::add(x12, x13), x23);
+        equations[j + 2] = field::sub(field::sub(field::neg(xs[i + 1]), xs[i + 2]), xs[i + 3]);
+        equations[j + 3] = 1;
+        inverses[i] = eval(&equations[j..(j + 4)], xs[i + 0]);
 
         // eq1
-        equations.push(field::mul(field::neg(x02), xs[3]));
-        equations.push(field::add(field::add(x02, x03), x23));
-        equations.push(field::sub(field::sub(field::neg(xs[0]), xs[2]), xs[3]));
-        equations.push(1);
-
-        inverses.push(eval(&equations[eq_idx..(eq_idx + 4)], xs[1]));
-        eq_idx += 4;
+        equations[j + 4] = field::mul(field::neg(x02), xs[i + 3]);
+        equations[j + 5] = field::add(field::add(x02, x03), x23);
+        equations[j + 6] = field::sub(field::sub(field::neg(xs[i + 0]), xs[i + 2]), xs[i + 3]);
+        equations[j + 7] = 1;
+        inverses[i + 1] = eval(&equations[(j + 4)..(j + 8)], xs[i + 1]);
 
         // eq2
-        equations.push(field::mul(field::neg(x01), xs[3]));
-        equations.push(field::add(field::add(x01, x03), x13));
-        equations.push(field::sub(field::sub(field::neg(xs[0]), xs[1]), xs[3]));
-        equations.push(1);
-
-        inverses.push(eval(&equations[eq_idx..(eq_idx + 4)], xs[2]));
-        eq_idx += 4;
+        equations[j +  8] = field::mul(field::neg(x01), xs[i + 3]);
+        equations[j +  9] = field::add(field::add(x01, x03), x13);
+        equations[j + 10] = field::sub(field::sub(field::neg(xs[i + 0]), xs[i + 1]), xs[i + 3]);
+        equations[j + 11] = 1;
+        inverses[i + 2] = eval(&equations[(j + 8)..(j + 12)], xs[i + 2]);
 
         // eq3
-        equations.push(field::mul(field::neg(x01), xs[2]));
-        equations.push(field::add(field::add(x01, x02), x12));
-        equations.push(field::sub(field::sub(field::neg(xs[0]), xs[1]), xs[2]));
-        equations.push(1);
-
-        inverses.push(eval(&equations[eq_idx..(eq_idx + 4)], xs[3]));
-        eq_idx += 4;
+        equations[j + 12] = field::mul(field::neg(x01), xs[i + 2]);
+        equations[j + 13] = field::add(field::add(x01, x02), x12);
+        equations[j + 14] = field::sub(field::sub(field::neg(xs[i + 0]), xs[i + 1]), xs[i + 2]);
+        equations[j + 15] = 1;
+        inverses[i + 3] = eval(&equations[(j + 12)..(j + 16)], xs[i + 3]);
     }
 
     let inverses = field::inv_many(&inverses);
 
-    let mut result: Vec<u64> = Vec::with_capacity(x_sets.len());
-    for i in (0..x_sets.len()).step_by(4) {
-        let ys = &y_sets[i..(i + 4)];
+    let mut result: Vec<u64> = Vec::with_capacity(n * 4);
+    unsafe { result.set_len(n * 4); }
 
-        let mut v = [0u64; 4];
-        for j in 0..4 {
-            let inv_y = field::mul(ys[j], inverses[i + j]);
+    for (i, j) in (0..ys.len()).step_by(4).zip((0..equations.len()).step_by(16)) {
+        
+        // iteration 0
+        let mut inv_y = field::mul(ys[i], inverses[i]);
+        result[i + 0] = field::mul(inv_y, equations[j + 0]);
+        result[i + 1] = field::mul(inv_y, equations[j + 1]);
+        result[i + 2] = field::mul(inv_y, equations[j + 2]);
+        result[i + 3] = field::mul(inv_y, equations[j + 3]);
 
-            v[0] = field::add(v[0], field::mul(inv_y, equations[(i + j) * 4]));
-            v[1] = field::add(v[1], field::mul(inv_y, equations[(i + j) * 4 + 1]));
-            v[2] = field::add(v[2], field::mul(inv_y, equations[(i + j) * 4 + 2]));
-            v[3] = field::add(v[3], field::mul(inv_y, equations[(i + j) * 4 + 3]));
-        }
+        // iteration 1
+        inv_y = field::mul(ys[i + 1], inverses[i + 1]);
+        result[i + 0] = field::add(result[i + 0], field::mul(inv_y, equations[j + 4]));
+        result[i + 1] = field::add(result[i + 1], field::mul(inv_y, equations[j + 5]));
+        result[i + 2] = field::add(result[i + 2], field::mul(inv_y, equations[j + 6]));
+        result[i + 3] = field::add(result[i + 3], field::mul(inv_y, equations[j + 7]));
 
-        result.push(v[0]);
-        result.push(v[1]);
-        result.push(v[2]);
-        result.push(v[3]);
+        // iteration 2
+        inv_y = field::mul(ys[i + 2], inverses[i + 2]);
+        result[i + 0] = field::add(result[i + 0], field::mul(inv_y, equations[j +  8]));
+        result[i + 1] = field::add(result[i + 1], field::mul(inv_y, equations[j +  9]));
+        result[i + 2] = field::add(result[i + 2], field::mul(inv_y, equations[j + 10]));
+        result[i + 3] = field::add(result[i + 3], field::mul(inv_y, equations[j + 11]));
+
+        // iteration 3
+        inv_y = field::mul(ys[i + 3], inverses[i + 3]);
+        result[i + 0] = field::add(result[i + 0], field::mul(inv_y, equations[j + 12]));
+        result[i + 1] = field::add(result[i + 1], field::mul(inv_y, equations[j + 13]));
+        result[i + 2] = field::add(result[i + 2], field::mul(inv_y, equations[j + 14]));
+        result[i + 3] = field::add(result[i + 3], field::mul(inv_y, equations[j + 15]));
     }
-
 
     return result;
 }
