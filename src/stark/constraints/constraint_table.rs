a9f77da816e5205f4811dd27a432320d1bb4a98b
bibbinth <bobbinth@protonmail.com>
2020-04-27 16:22:41 -0700

    moved transition constraint extension to synthetic division

--- src/stark/constraints/constraint_table.rs
+++ src/stark/constraints/constraint_table.rs
@@ -32,10 +32,10 @@ impl ConstraintTable {
         return ConstraintTable {
             evaluator       : evaluator,
             domain          : evaluation_domain,
-            trace_reg_comb  : zero_filled_vector(composition_domain_size, evaluation_domain_size),
-            init_bound_comb : zero_filled_vector(composition_domain_size, evaluation_domain_size),
-            final_bound_comb: zero_filled_vector(composition_domain_size, evaluation_domain_size),
-            transition_comb : zero_filled_vector(composition_domain_size, evaluation_domain_size),
+            trace_reg_comb  : uninit_vector(composition_domain_size),
+            init_bound_comb : uninit_vector(composition_domain_size),
+            final_bound_comb: uninit_vector(composition_domain_size),
+            transition_comb : uninit_vector(composition_domain_size),
             domain_stride   : domain_stride,
         };
     }
@@ -54,6 +54,14 @@ impl ConstraintTable {
 
     pub fn domain_stride(&self) -> usize {
         return self.domain_stride;
+    }
+
+    pub fn composition_domain_size(&self) -> usize {
+        return self.evaluator.domain_size();
+    }
+
+    pub fn trace_length(&self) -> usize {
+        return self.evaluator.trace_length();
     }
 
     // CONSTRAINT EVALUATION
@@ -76,69 +84,42 @@ impl ConstraintTable {
         let composition_root = field::get_root_of_unity(self.domain.len() as u64);
         let inv_twiddles = fft::get_inv_twiddles(composition_root, self.domain.len());
 
-        let domain_root = field::get_root_of_unity(self.domain.len() as u64);
-        let twiddles = fft::get_twiddles(domain_root, self.domain.len());
-
         // 1 ----- trace register combination -----------------------------------------------------
-        // extend linear combination of trace registers to the full evaluation domain
-        extend_evaluations(&mut self.trace_reg_comb, &inv_twiddles, &twiddles);
-        let mut result = uninit_vector(self.domain.len());
-        result.copy_from_slice(&self.trace_reg_comb);
+        // interpolate linear combination of trace registers into a polynomial, and copy the
+        // polynomial into the result
+        polys::interpolate_fft_twiddles(&mut self.trace_reg_comb, &inv_twiddles, true);
+        
+        let mut result = zero_filled_vector(self.composition_domain_size(), self.domain.len());
+        result[0..self.composition_domain_size()].copy_from_slice(&self.trace_reg_comb);
 
         // 2 ----- boundary constraints for the initial step --------------------------------------
-        // divide constraint evaluations by Z(x) = (x - 1), extend them to evaluation domain, and
-        // add them to the result
+        // interpolate initial step boundary constraint combination into a polynomial, divide the 
+        // polynomial by Z(x) = (x - 1), and add it to the result
         polys::interpolate_fft_twiddles(&mut self.init_bound_comb, &inv_twiddles, true);
         polys::syn_div_in_place(&mut self.init_bound_comb, field::neg(field::ONE));
-        unsafe { self.init_bound_comb.set_len(self.init_bound_comb.capacity()); }
-        polys::eval_fft_twiddles(&mut self.init_bound_comb, &twiddles, true);
         parallel::add_in_place(&mut result, &self.init_bound_comb, 1);
 
         // 3 ----- boundary constraints for the final step ----------------------------------------
-        // divide constraint evaluations by Z(x) = (x - x_at_last_step), extend them to evaluation
-        // domain, and add them to the result
+        // interpolate final step boundary constraint combination into a polynomial, divide the 
+        // polynomial by Z(x) = (x - x_at_last_step), and add it to the result
         polys::interpolate_fft_twiddles(&mut self.final_bound_comb, &inv_twiddles, true);
         let x_at_last_step = self.get_x_at_last_step();
         polys::syn_div_in_place(&mut self.final_bound_comb, field::neg(x_at_last_step));
-        unsafe { self.final_bound_comb.set_len(self.final_bound_comb.capacity()); }
-        polys::eval_fft_twiddles(&mut self.final_bound_comb, &twiddles, true);
         parallel::add_in_place(&mut result, &self.final_bound_comb, 1);
 
         // 4 ----- transition constraints ---------------------------------------------------------
-        // extend constraint evaluations to the full evaluation domain, divide them by zero poly
-        // Z(x) = (x^steps - 1) / (x - x_at_last_step), and add them to the result
-        extend_evaluations(&mut self.transition_comb, &inv_twiddles, &twiddles);
-        let z_inverses = self.get_transition_inv_z();
-        parallel::mul_in_place(&mut self.transition_comb, &z_inverses, 1);
+        // interpolate transition constraint combination into a polynomial, divide the polynomial
+        // by Z(x) = (x^steps - 1) / (x - x_at_last_step), and add it to the result
+        let trace_length = self.trace_length();
+        polys::interpolate_fft_twiddles(&mut self.transition_comb, &inv_twiddles, true);
+        polys::syn_div_expanded_in_place(&mut self.transition_comb, trace_length, &[x_at_last_step]);
         parallel::add_in_place(&mut result, &self.transition_comb, 1);
 
-        return result;
-    }
-
-    // ZERO POLYNOMIALS
-    // -------------------------------------------------------------------------------------------
-    
-    /// Computes inverse evaluations of Z(x) polynomial for transition constraints; Z(x) = 
-    /// (x^steps - 1) / (x - x_at_last_step), so, inv(Z(x)) = inv(x^steps - 1) * (x - x_at_last_step)
-    fn get_transition_inv_z(&self) -> Vec<u64> {
-
-        // compute (x - x_at_last_step)
-        let mut last_step_z = self.domain.clone();
-        parallel::sub_const_in_place(&mut last_step_z, self.get_x_at_last_step(), 1);
-
-        // compute (x^steps - 1); TODO: can be parallelized
-        let steps = self.evaluator.trace_length();
-        let mut result = uninit_vector(self.domain.len());
-        for i in 0..result.len() {
-            let x_to_the_steps = self.domain[(i * steps) % self.domain.len()];
-            result[i] = field::sub(x_to_the_steps, field::ONE);
-        }
-
-        // invert the numerators
-        let mut result = parallel::inv(&result, 1);
-
-        // multiply the result by (x - x_at_last_step)
-        parallel::mul_in_place(&mut result, &last_step_z, 1);
+        // 5 ----- evaluate combination polynomial on evaluation domain ---------------------------
+        let domain_root = field::get_root_of_unity(self.domain.len() as u64);
+        let twiddles = fft::get_twiddles(domain_root, self.domain.len());
+        unsafe { result.set_len(result.capacity()); }
+        polys::eval_fft_twiddles(&mut result, &twiddles, true);
 
         return result;
     }
@@ -146,15 +127,7 @@ impl ConstraintTable {
     // HELPER METHODS
     // -------------------------------------------------------------------------------------------
     fn get_x_at_last_step(&self) -> u64 {
-        let extension_factor = self.domain.len() / self.evaluator.trace_length();
+        let extension_factor = self.domain.len() / self.trace_length();
         return self.domain[self.domain.len() - extension_factor];
     }
-}
-
-// HELPER FUNCTIONS
-// ================================================================================================
-fn extend_evaluations(mut evaluations: &mut Vec<u64>, inv_twiddles: &[u64], twiddles: &[u64]) {
-    polys::interpolate_fft_twiddles(&mut evaluations, &inv_twiddles, true);
-    unsafe { evaluations.set_len(evaluations.capacity()); }
-    polys::eval_fft_twiddles(&mut evaluations, &twiddles, true);
 }
