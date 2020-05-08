179ca6c08c3f3874f71ce5ade15813b1bf7a80b3
bibbinth <bobbinth@protonmail.com>
2020-05-07 21:04:43 -0700

    some clean-up in verifier module

--- src/stark/verifier.rs
+++ src/stark/verifier.rs
@@ -1,7 +1,5 @@
-use crate::{ math::field, crypto::{ MerkleTree, BatchMerkleProof }, utils::CopyInto };
-use super::{
-    TraceState, StarkProof, ConstraintEvaluator, fri, utils::compute_query_positions,
-    CompositionCoefficients, MAX_CONSTRAINT_DEGREE };
+use crate::{ math::field, crypto::{ MerkleTree }, utils::CopyInto };
+use super::{ StarkProof, TraceState, ConstraintEvaluator, CompositionCoefficients, fri, utils };
 
 // VERIFIER FUNCTION
 // ================================================================================================
@@ -9,9 +7,9 @@ use super::{
 pub fn verify(program_hash: &[u8; 32], inputs: &[u64], outputs: &[u64], proof: &StarkProof) -> Result<bool, String> {
 
     let options = proof.options();
+    let hash_fn = options.hash_function();
     let lde_domain_size = proof.domain_size();
     let lde_root = field::get_root_of_unity(lde_domain_size as u64);
-    let degree_proof = proof.degree_proof();
 
     // 1 ----- Verify deep point evaluation -------------------------------------------------------
     let constraint_evaluation_at_z = evaluate_constraints(
@@ -26,6 +24,7 @@ pub fn verify(program_hash: &[u8; 32], inputs: &[u64], outputs: &[u64], proof: &
     }
 
     // 2 ----- Verify proof of work and determine query positions ---------------------------------
+    let degree_proof = proof.degree_proof();
     let mut fri_roots: Vec<u64> = Vec::new();
     for layer in degree_proof.layers.iter() {
         layer.root.iter().for_each(|&v| fri_roots.push(v));
@@ -33,79 +32,33 @@ pub fn verify(program_hash: &[u8; 32], inputs: &[u64], outputs: &[u64], proof: &
     degree_proof.rem_root.iter().for_each(|&v| fri_roots.push(v));
 
     let mut seed = [0u64; 4];
-    options.hash_function()(&fri_roots, &mut seed);
+    hash_fn(&fri_roots, &mut seed);
     // TODO: verify proof of work
 
-    let positions = compute_query_positions(&seed, lde_domain_size, options);
+    let t_positions = utils::compute_query_positions(&seed, lde_domain_size, options);
+    let c_positions = utils::map_trace_to_constraint_positions(&t_positions);
 
     // 3 ----- Verify trace and constraint Merkle proofs ------------------------------------------
-    
-    // verify the trace proof
-    if !MerkleTree::verify_batch(proof.trace_root(), &positions, &proof.trace_proof(), options.hash_function()) {
+    if !MerkleTree::verify_batch(proof.trace_root(), &t_positions, &proof.trace_proof(), hash_fn) {
         return Err(String::from("verification of trace Merkle proof failed"));
     }
-    // verify the constraint proof
-    let mut constraint_positions = Vec::with_capacity(positions.len());
-    for &position in positions.iter() {
-        let cp = position / 4;
-        if !constraint_positions.contains(&cp) {
-            constraint_positions.push(cp);
-        }
-    }
 
-    if !MerkleTree::verify_batch(proof.constraint_root(), &constraint_positions, &proof.constraint_proof(), options.hash_function()) {
+    if !MerkleTree::verify_batch(proof.constraint_root(), &c_positions, &proof.constraint_proof(), hash_fn) {
         return Err(String::from("verification of constraint Merkle proof failed"));
     }
 
     // 4 ----- Compute composition values ---------------------------------------------------------
     
     let z = field::prng(proof.constraint_root().copy_into());
-    let g = field::get_root_of_unity(proof.trace_length() as u64);
-    let next_z = field::mul(z, g);
     let coefficients = CompositionCoefficients::new(proof.constraint_root());
 
-    let t_incremental_degree = (get_composition_degree(proof.trace_length()) - (proof.trace_length() - 2)) as u64;
-    let trace_at_z1 = proof.get_state_at_z().registers().to_vec();
-    let trace_at_z2 = proof.get_state_at_next_z().registers().to_vec();
-    let trace_states = proof.trace_evaluations();
-
-    let c_evaluations = get_constraint_evaluations(&proof.constraint_proof(), &positions, &constraint_positions);
-
-    let mut evaluations = Vec::new();
-    for (i, &position) in positions.iter().enumerate() {
-        let x = field::exp(lde_root, position as u64);
-        let registers = &trace_states[i];
-
-        let mut t1_composition = 0;
-        let mut t2_composition = 0;
-
-        for (j, &value) in registers.iter().enumerate() {
-            // compute T1(x) = (T(x) - T(z)) / (x - z), multiply it by a pseudo-random coefficient
-            let t1 = field::div(field::sub(value, trace_at_z1[j]), field::sub(x, z));
-            t1_composition = field::add(t1_composition, field::mul(t1, coefficients.trace1[j]));
-
-            // compute T2(x) = (T(x) - T(z * g)) / (x - z * g), multiply it by a pseudo-random
-            let t2 = field::div(field::sub(value, trace_at_z2[j]), field::sub(x, next_z));
-            t2_composition = field::add(t2_composition, field::mul(t2, coefficients.trace2[j]));
-        }
-
-        let mut t_composition = field::add(t1_composition, t2_composition);
-
-        let xp = field::exp(x, t_incremental_degree);
-        let t2_composition_adj = field::mul(field::mul(t_composition, xp), coefficients.t2_degree);
-        t_composition = field::add(field::mul(t_composition, coefficients.t1_degree), t2_composition_adj);
-
-        // compute C(x) = (P(x) - P(z)) / (x - z)
-        let mut c_composition = field::sub(c_evaluations[i], proof.get_constraint_evaluation_at_z());
-        c_composition = field::div(c_composition, field::sub(x, z));
-        c_composition = field::mul(c_composition, coefficients.constraints);
-
-        evaluations.push(field::add(t_composition, c_composition));
-    }
+    let t_composition = compose_registers(&proof, &t_positions, z, &coefficients);
+    let c_composition = compose_constraints(&proof, &t_positions, &c_positions, z, &coefficients);
+    let evaluations = t_composition.iter().zip(c_composition).map(|(&t, c)| field::add(t, c)).collect::<Vec<u64>>();
     
     // 5 ----- Verify log-degree proof -------------------------------------------------------------
-    let composition_degree_plus_1 = get_composition_degree(proof.trace_length()) + 1;
-    return match fri::verify(&degree_proof, &evaluations, &positions, lde_root, composition_degree_plus_1, options) {
+    let composition_degree_plus_1 = utils::get_composition_degree(proof.trace_length()) + 1;
+    return match fri::verify(&degree_proof, &evaluations, &t_positions, lde_root, composition_degree_plus_1, options) {
         Ok(result) => Ok(result),
         Err(msg) => Err(format!("verification of low-degree proof failed: {}", msg))
     }
@@ -133,18 +86,75 @@ fn evaluate_constraints(evaluator: ConstraintEvaluator, state1: TraceState, stat
     return result;
 }
 
-// TODO: move to utils
-fn get_composition_degree(trace_length: usize) -> usize {
-    return (MAX_CONSTRAINT_DEGREE - 1) * trace_length - 1;
+fn compose_registers(proof: &StarkProof, positions: &[usize], z: u64, cc: &CompositionCoefficients) -> Vec<u64> {
+    let mut result = Vec::new();
+
+    let trace_length = proof.trace_length();
+    let lde_domain_size = proof.domain_size();
+    let evaluations = proof.trace_evaluations();
+
+    let lde_root = field::get_root_of_unity(lde_domain_size as u64);
+    let trace_root = field::get_root_of_unity(trace_length as u64);
+    let next_z = field::mul(z, trace_root);
+
+    let trace_at_z1 = proof.get_state_at_z().registers().to_vec();
+    let trace_at_z2 = proof.get_state_at_next_z().registers().to_vec();
+
+    let t_incremental_degree = (utils::get_composition_degree(trace_length) - (trace_length - 2)) as u64;
+
+    for (i, &position) in positions.iter().enumerate() {
+        let x = field::exp(lde_root, position as u64);
+        let registers = &evaluations[i];
+
+        let mut t1_composition = 0;
+        let mut t2_composition = 0;
+
+        for (j, &value) in registers.iter().enumerate() {
+            // compute T1(x) = (T(x) - T(z)) / (x - z), multiply it by a pseudo-random coefficient
+            let t1 = field::div(field::sub(value, trace_at_z1[j]), field::sub(x, z));
+            t1_composition = field::add(t1_composition, field::mul(t1, cc.trace1[j]));
+
+            // compute T2(x) = (T(x) - T(z * g)) / (x - z * g), multiply it by a pseudo-random
+            let t2 = field::div(field::sub(value, trace_at_z2[j]), field::sub(x, next_z));
+            t2_composition = field::add(t2_composition, field::mul(t2, cc.trace2[j]));
+        }
+
+        let mut t_composition = field::add(t1_composition, t2_composition);
+
+        let xp = field::exp(x, t_incremental_degree);
+        let t2_composition_adj = field::mul(field::mul(t_composition, xp), cc.t2_degree);
+        t_composition = field::add(field::mul(t_composition, cc.t1_degree), t2_composition_adj);
+
+        result.push(t_composition);
+    }
+
+    return result;
 }
 
-fn get_constraint_evaluations(proof: &BatchMerkleProof, positions: &[usize], constraint_positions: &[usize]) -> Vec<u64> {
-    let values = &proof.values;
+fn compose_constraints(proof: &StarkProof, t_positions: &[usize], c_positions: &[usize], z: u64, cc: &CompositionCoefficients) -> Vec<u64> {
+
+    // build constraint evaluation values from the leaves of constraint Merkle proof
+    let mut evaluations = Vec::new();
+    let leaves = proof.constraint_proof().values;
+    for &position in t_positions.iter() {
+        let idx = c_positions.iter().position(|&v| v == position / 4).unwrap();
+        let value = leaves[idx][position % 4];
+        evaluations.push(value);
+    }
+
+    let lde_root = field::get_root_of_unity(proof.domain_size() as u64);
+    let evaluation_at_z = proof.get_constraint_evaluation_at_z();
+
+    // divide out deep point from the evaluations
     let mut result = Vec::new();
-    for &position in positions.iter() {
-        let idx = constraint_positions.iter().position(|&v| v == position / 4).unwrap();
-        let value = values[idx][position % 4];
-        result.push(value);
+    for (evaluation, &position) in evaluations.into_iter().zip(t_positions) {
+        let x = field::exp(lde_root, position as u64);
+
+        // compute C(x) = (P(x) - P(z)) / (x - z)
+        let composition = field::div(field::sub(evaluation, evaluation_at_z), field::sub(x, z));
+
+        // multiply by pseudo-random coefficient for linear combination
+        result.push(field::mul(composition, cc.constraints));
     }
 
     return result;
