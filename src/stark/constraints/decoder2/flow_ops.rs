24ae20d9c1852d84c67b013e6e0c91afcab8783d
bibbinth <bobbinth@protonmail.com>
2020-07-17 21:22:28 -0700

    added constraints for loop operations

--- src/stark/constraints/decoder2/flow_ops.rs
+++ src/stark/constraints/decoder2/flow_ops.rs
@@ -16,7 +16,8 @@ pub fn enforce_begin(result: &mut [u128], current: &TraceState, next: &TraceStat
     let parent_hash = current.sponge()[0];
     let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
     let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
-    enforce_stack_push(ctx_result, current.ctx_stack(), next.ctx_stack(), parent_hash, op_flag);
+    ctx_result.agg_constraint(0, op_flag, are_equal(parent_hash, next.ctx_stack()[0]));
+    enforce_stack_right_shift(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
 
     // make sure loop stack didn't change
     let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
@@ -37,7 +38,7 @@ pub fn enforce_tend(result: &mut [u128], current: &TraceState, next: &TraceState
     // make parent hash was popped from context stack
     let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
     let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
-    enforce_stack_pop(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
+    enforce_stack_left_shift(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
 
     // make sure loop stack didn't change
     let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
@@ -58,7 +59,7 @@ pub fn enforce_fend(result: &mut [u128], current: &TraceState, next: &TraceState
     // make sure parent hash was popped from context stack
     let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
     let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
-    enforce_stack_pop(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
+    enforce_stack_left_shift(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
 
     // make sure loop stack didn't change
     let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
@@ -67,17 +68,68 @@ pub fn enforce_fend(result: &mut [u128], current: &TraceState, next: &TraceState
 
 pub fn enforce_loop(result: &mut [u128], current: &TraceState, next: &TraceState, op_flag: u128)
 {
-    // TODO
+    // make sure sponge state has been cleared
+    let next_sponge = next.sponge();
+    result.agg_constraint(0, op_flag, is_zero(next_sponge[0]));
+    result.agg_constraint(1, op_flag, is_zero(next_sponge[1]));
+    result.agg_constraint(2, op_flag, is_zero(next_sponge[2]));
+    result.agg_constraint(3, op_flag, is_zero(next_sponge[3]));
+
+    // make sure hash of parent block was pushed onto the context stack
+    let parent_hash = current.sponge()[0];
+    let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
+    let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
+    ctx_result.agg_constraint(0, op_flag, are_equal(parent_hash, next.ctx_stack()[0]));
+    enforce_stack_right_shift(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
+
+    // make sure loop stack was shifted by 1 item to the right, but don't enforce constraints
+    // on the first item of the stack (which will contain loop image)
+    let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
+    enforce_stack_right_shift(loop_result, current.loop_stack(), next.loop_stack(), op_flag);
 }
 
 pub fn enforce_wrap(result: &mut [u128], current: &TraceState, next: &TraceState, op_flag: u128)
 {
+    // make sure sponge state has been cleared
+    let next_sponge = next.sponge();
+    result.agg_constraint(0, op_flag, is_zero(next_sponge[0]));
+    result.agg_constraint(1, op_flag, is_zero(next_sponge[1]));
+    result.agg_constraint(2, op_flag, is_zero(next_sponge[2]));
+    result.agg_constraint(3, op_flag, is_zero(next_sponge[3]));
+
+    // make sure item at the top of loop stack is equal to loop image
     // TODO
+
+    // make sure context stack didn't change
+    let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
+    let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
+    enforce_stack_copy(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
+
+    // make sure loop stack didn't change
+    let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
+    enforce_stack_copy(loop_result, current.loop_stack(), next.loop_stack(), op_flag);
 }
 
 pub fn enforce_break(result: &mut [u128], current: &TraceState, next: &TraceState, op_flag: u128)
 {
+    // make sure sponge state didn't change
+    let old_sponge = current.sponge();
+    let new_sponge = next.sponge();
+    for i in 0..SPONGE_WIDTH {
+        result.agg_constraint(i, op_flag, are_equal(old_sponge[i], new_sponge[i]));
+    }
+
+    // make sure item at the top of loop stack is equal to loop image
     // TODO
+
+    // make sure context stack didn't change
+    let ctx_stack_end = SPONGE_WIDTH + current.ctx_stack().len();
+    let ctx_result = &mut result[SPONGE_WIDTH..ctx_stack_end];
+    enforce_stack_copy(ctx_result, current.ctx_stack(), next.ctx_stack(), op_flag);
+
+    // make loop image was popped from loop stack
+    let loop_result = &mut result[ctx_stack_end..ctx_stack_end + current.loop_stack().len()];
+    enforce_stack_left_shift(loop_result, current.loop_stack(), next.loop_stack(), op_flag);
 }
 
 pub fn enforce_void(result: &mut [u128], current: &TraceState, next: &TraceState, op_flag: u128)
@@ -99,12 +151,13 @@ pub fn enforce_void(result: &mut [u128], current: &TraceState, next: &TraceState
     enforce_stack_copy(loop_result, current.loop_stack(), next.loop_stack(), op_flag);
 }
 
-
 // HELPER FUNCTIONS
 // ================================================================================================
 
-fn enforce_stack_pop(result: &mut [u128], old_stack: &[u128], new_stack: &[u128], op_flag: u128)
+fn enforce_stack_left_shift(result: &mut [u128], old_stack: &[u128], new_stack: &[u128], op_flag: u128)
 {
+    if result.len() == 0 { return; }
+
     let last_idx = result.len() - 1;
     for i in 0..last_idx {
         result.agg_constraint(i, op_flag, are_equal(old_stack[i + 1], new_stack[i]));
@@ -113,10 +166,8 @@ fn enforce_stack_pop(result: &mut [u128], old_stack: &[u128], new_stack: &[u128]
     result.agg_constraint(last_idx, op_flag, is_zero(new_stack[last_idx]));
 }
 
-fn enforce_stack_push(result: &mut [u128], old_stack: &[u128], new_stack: &[u128], push_value: u128, op_flag: u128)
+fn enforce_stack_right_shift(result: &mut [u128], old_stack: &[u128], new_stack: &[u128], op_flag: u128)
 {
-    result.agg_constraint(0, op_flag, are_equal(push_value, new_stack[0]));
-    
     for i in 1..result.len() {
         result.agg_constraint(i, op_flag, are_equal(old_stack[i - 1], new_stack[i]));
     }
@@ -243,6 +294,21 @@ mod tests {
         let mut evaluations = vec![0; 6];
         super::enforce_fend(&mut evaluations, &state1, &state2, 1);
         assert_eq!(vec![3, 0, 1, 4, 1, 6], evaluations);
+    }
+
+    #[test]
+    fn op_loop() {
+        // TODO
+    }
+
+    #[test]
+    fn op_wrap() {
+        // TODO
+    }
+
+    #[test]
+    fn op_break() {
+        // TODO
     }
 
     #[test]
